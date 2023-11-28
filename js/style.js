c6cc8b620f5041f681308b9276bea41f5bc588bf
Hamid Kamyab <mr_kamyab@yahoo.com>
2023-11-28 22:11:00 +0330

    Complete part of the template header

--- js/style.js
+++ js/style.js
@@ -1,1 +1,36 @@
-﻿
+﻿var timeout;
+$('.header-cartBox').on('mouseenter', function() {
+    visibleTag('.header-cart', 'show')
+});
+
+$(".header-cartBox").on("mouseleave", function() {
+    visibleTag('.header-cart', 'hidden')
+});
+
+$('.header-cart').on('mouseenter', function() {
+    clearTimeout(timeout);
+});
+
+$(".header-cart").on("mouseleave", function() {
+    visibleTag('.header-cart', 'hidden')
+});
+
+$('.header-userBox').on('mouseenter', function() {
+    visibleTag('.header-user-msg', 'show')
+});
+
+$('.header-userBox').on("mouseleave", function() {
+    visibleTag('.header-user-msg', 'hidden')
+});
+
+function visibleTag(tag, status = '') {
+    if (status == 'show') {
+        $(tag).fadeIn();
+    }
+    if (status == 'hidden') {
+        timeout = setTimeout(() => {
+            $(tag).fadeOut();
+        }, 1000);
+    }
+
+}
